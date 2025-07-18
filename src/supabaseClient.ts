b941926d9ce54223b07a9f1543ae413f3faa66ba
Lucas Moura <lucasmoura53@gmail.com>
2025-07-17 21:06:29 -0300

    final dia

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Substitua com suas chaves do Supabase. Use 'string' para tipar as variáveis.
-const supabaseUrl: string = 'https://ogwawiodxxwkshvdpudb.supabase.co';
-const supabaseAnonKey: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9nd2F3aW9keHh3a3NodmRwdWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NTMxMjIsImV4cCI6MjA2ODMyOTEyMn0.FxzyJHMEWf6Pq6Ox_1PyF3LwQLlu-eUxLapcJbdf1W4';
+const supabaseUrl: string = '';
+const supabaseAnonKey: string = '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
